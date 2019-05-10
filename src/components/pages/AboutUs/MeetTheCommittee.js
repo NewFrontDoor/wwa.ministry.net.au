b1332ec559b56960dea390214421cd4826ea63ea
Bratalix <bratalix@gmail.com>
2019-05-10 14:07:25 +1000

    Update committee

--- src/components/pages/AboutUs/MeetTheCommittee.js
+++ src/components/pages/AboutUs/MeetTheCommittee.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import Person from '../../models/Person';
 
-import Cristiane from '../../../assets/img/Baker,Cristiane.png';
 import Rachel from '../../../assets/img/Bain,Rachel.png';
 import Libby from '../../../assets/img/Dilger,Libby.png';
 import Elya from '../../../assets/img/Richardson,Elya.png';
@@ -14,21 +13,20 @@ import Blank from '../../../assets/img/Blank.png'
 
 export default () => (
     <section className="container">
-    <div className="section_header">
-        <h3>Meet the Committee</h3>
-    </div>
+        <div className="section_header">
+            <h3>Meet the Committee</h3>
+        </div>
 
-    <div className="text-center">
-    <Person name="Cristiane Baker" title="Coordinator" image={Cristiane} />
-    <Person name="Rachel Bain" title="Coordinator" image={Rachel} />
-    <Person name="Libby Dilger" title="Admin" image={Libby} />
-    <Person name="Elya Richardson" title="Workshop & Media" image={Elya} />
+        <div className="text-center">
+            <Person name="Rachel Bain" title="Coordinator" image={Rachel} />
+            <Person name="Libby Dilger" title="Admin" image={Libby} />
+            <Person name="Elya Richardson" title="Workshop & Media" image={Elya} />
 
-    <Person name="Lyndal Jolly" title="Communications" image={Lyndal} />
-    <Person name="Jane Maarseveen" title="Missions" image={Jane} />
-    <Person name="Naomi Petrie" title="Activities" image={Blank}/>
- 
+            <Person name="Lyndal Jolly" title="Communications" image={Lyndal} />
+            <Person name="Jane Maarseveen" title="Missions" image={Jane} />
+            <Person name="Naomi Petrie" title="Activities" image={Blank} />
 
-    </div>
-</section>
+
+        </div>
+    </section>
 )
