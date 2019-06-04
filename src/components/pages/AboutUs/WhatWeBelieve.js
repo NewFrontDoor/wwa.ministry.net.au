979bbd7e456fcd495cb2aedbf68032bf412edd1a
Bratalix <bratalix@gmail.com>
2019-06-04 13:41:18 +1000

    putting in col

--- src/components/pages/AboutUs/WhatWeBelieve.js
+++ src/components/pages/AboutUs/WhatWeBelieve.js
@@ -8,37 +8,38 @@ export default () => (
         <div className="section_header">
             <h3>What We Believe</h3>
         </div>
-
-        <h3>We Are Christians Who Believe the Bible</h3>
-        <p>
-            We believe that the whole Bible is the Word of God in written form, that the Bible is not mistaken in anything it says, that it is the final authority for the church's beliefs and practices, and the ultimate guide for all mankind. Therefore we believe that hearing God's Word carries with it the responsibility to obey God's will. To help us understand the Bible we accept and use the Westminster Confession of Faith as a reasonable summary of the Bible's teaching.
+        <div className="col-md-12">
+            <h3>We Are Christians Who Believe the Bible</h3>
+            <p>
+                We believe that the whole Bible is the Word of God in written form, that the Bible is not mistaken in anything it says, that it is the final authority for the church's beliefs and practices, and the ultimate guide for all mankind. Therefore we believe that hearing God's Word carries with it the responsibility to obey God's will. To help us understand the Bible we accept and use the Westminster Confession of Faith as a reasonable summary of the Bible's teaching.
         </p>
-        <br />
+            <br />
 
-        <h3>We Are Christians Who Proclaim God's Promises</h3>
+            <h3>We Are Christians Who Proclaim God's Promises</h3>
 
-        <p>
-            God gave mankind a message of Good News ('the Gospel') through the life, death and resurrection of Jesus Christ, a message which demands a response from all people everywhere.
+            <p>
+                God gave mankind a message of Good News ('the Gospel') through the life, death and resurrection of Jesus Christ, a message which demands a response from all people everywhere.
         </p>
-        <p>
-            We seek to proclaim the good news that when people put their trust in Jesus Christ and obey His commands their sins are forgiven, they live under God's blessing day by day, and they receive the assurance of eternal life.
+            <p>
+                We seek to proclaim the good news that when people put their trust in Jesus Christ and obey His commands their sins are forgiven, they live under God's blessing day by day, and they receive the assurance of eternal life.
         </p>
-        <br />
+            <br />
 
-        <h3>We Are Practical Christians</h3>
+            <h3>We Are Practical Christians</h3>
 
-        <p>
-            We aim to provide opportunities for people of all ages to learn how to respond to this good news, and then to become effective followers of Jesus Christ. As they do this they progress in the Christian faith through Bible study, prayer, fellowship with other Christians, and service to others.
+            <p>
+                We aim to provide opportunities for people of all ages to learn how to respond to this good news, and then to become effective followers of Jesus Christ. As they do this they progress in the Christian faith through Bible study, prayer, fellowship with other Christians, and service to others.
         </p>
-        <br />
+            <br />
 
-        <h3>We Are Relevant Christians</h3>
+            <h3>We Are Relevant Christians</h3>
 
-        <p>
-            We believe that the Bible's message provides tried and true answers for every modern problem and issue. As we worship God we seek to maintain a simple format with emphasis on the practical teaching and application of God's Word, the Bible, to the needs of our world.
+            <p>
+                We believe that the Bible's message provides tried and true answers for every modern problem and issue. As we worship God we seek to maintain a simple format with emphasis on the practical teaching and application of God's Word, the Bible, to the needs of our world.
         </p>
-        <br />
+            <br />
 
-        <p style={{ fontSize: "14px" }}>(From the <a href="http://pctas.org.au/" target="_blank" rel="noreferrer noopener">Statement of belief from Presbyterian of Tasmania</a>)</p>
+            <p style={{ fontSize: "14px" }}>(From the <a href="http://pctas.org.au/" target="_blank" rel="noreferrer noopener">Statement of belief from Presbyterian of Tasmania</a>)</p>
+        </div>
     </section>
 )
