339ce5adf6628af43b9192edb95987477887b54f
Bratalix <bratalix@gmail.com>
2019-07-05 19:21:38 +1000

    Updated address

--- src/components/registration/confirmations/ChequeDD.js
+++ src/components/registration/confirmations/ChequeDD.js
@@ -25,9 +25,9 @@ class ChequeDD extends Component {
           Reference: WWA {this.state.surname ? this.state.surname : '(Your Surname)'}<br /><br />
           <strong>Cheque:</strong><br />
           Please make your cheque payable to 'Presbyterian Church of Tasmania' and then post it to:<br />
-          Katinka Clack<br />
-          5 Alicia Road<br />
-          Kingston TAS 7050<br />
+          Libby Dilger<br />
+          40 Phoenix St<br />
+          Howrah TAS 7018<br />
           You will also receive a separate registration email, containing some of the things you need to know about the upcoming camp. <br /><br />
 
           Until then, stay up to date on our Facebook Page (@WWATasmania). We look forward to seeing you soon!<br />
