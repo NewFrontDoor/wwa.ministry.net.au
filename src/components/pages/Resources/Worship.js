fabd728f574f268a5dbb0f90c345dbc87a862a1e
Bratalix <bratalix@gmail.com>
2019-09-26 14:12:03 +1000

    Requested changes

--- src/components/pages/Resources/Worship.js
+++ src/components/pages/Resources/Worship.js
@@ -18,25 +18,33 @@ class Worship extends Component {
                 </p>
                 <br /><br />
                 <p>Every year during our weekend together we enjoy a time of singing together.</p>
-                <p>Below is a list of the songs that we've sung and if you want to keep enjoying the songs that we've sung you can <a href="https://open.spotify.com/playlist/03eHthhMbnJz5gn6KuWUxT">subscribe to our Spotify Playlist</a></p>
+                <p>Below is a list of the songs that we've sung during our weekend together. If you want to keep enjoying the music you can <a href="https://open.spotify.com/playlist/03eHthhMbnJz5gn6KuWUxT">subscribe to our Spotify Playlist</a></p>
                 <br /><br />
                 <table class="table">
-<thead><tr class="firstrow"><th>Song</th><th>By</th></tr></thead><tbody>
- <tr><td>10,000 Reasons (Bless The Lord)</td><td>Jonas Myrin; Matt Redman</td></tr>
- <tr><td>Behold Our God</td><td>Jonathan Baird; Meghan Baird; Ryan Baird; Stephen Altrogge</td></tr>
- <tr><td>Blessed Be Your Name</td><td>Beth Redman; Matt Redman</td></tr>
- <tr><td>In Christ Alone</td><td>Keith Getty; Stuart Townend</td></tr>
- <tr><td>In His Name</td><td>Charles Wesley; Gavin Perkins; Luke Woodhouse; Nathan Walter</td></tr>
- <tr><td>Just As I Am</td><td>Charlotte Elliott; David Moffitt; Sue C. Smith; Travis Cottrell; William Batchelder Bradbury</td></tr>
- <tr><td>Rock Of Ages</td><td>Augustus Montague Toplady; arr Ruth Buchanan</td></tr>
- <tr><td>Speak O Lord</td><td>Keith Getty; Stuart Townend</td></tr>
- <tr><td>There Is A Redeemer</td><td>Melody Green</td></tr>
- <tr><td>This Life I Live</td><td>Michael Morrow</td></tr>
- <tr><td>Undivided</td><td>Rob Smith</td></tr>
- <tr><td>What A Friend We Have In Jesus</td><td>Charles Crozat Converse; David Paxton; Joseph Medlicott Scriven</td></tr>
- <tr><td>You Gave Your Life (If Christ be God)</td><td>Steve Tillbrook</td></tr>
- <tr><td>You Never Change</td><td> Rob Smith</td></tr>
-</tbody></table>
+                    <thead><tr class="firstrow"><th>Song</th><th>By</th></tr></thead><tbody>
+                        <tr><td>10,000 Reasons (Bless The Lord)</td><td>Jonas Myrin; Matt Redman</td></tr>
+                        <tr><td>All I Have is Christ</td><td>Jordan Kauflin</td></tr>
+                        <tr><td>Behold Our God</td><td>Jonathan Baird; Meghan Baird; Ryan Baird; Stephen Altrogge</td></tr>
+                        <tr><td>Blessed Be Your Name</td><td>Beth Redman; Matt Redman</td></tr>
+                        <tr><td>Cornerstone</td><td>Edward Mote, Eric Liljero, Jonas Myrin, Reuben Morgan & William Batchelder Bradbury</td></tr>
+                        <tr><td>Forever</td><td>Chris Tomlin</td></tr>
+                        <tr><td>Here I am to Worship</td><td>Tim Hughes</td></tr>
+                        <tr><td>Here I Bow</td><td>Brian Johnson, Jason Ingram, Jenn Johnson, Jonas Myrin & Matt Redman</td></tr>
+                        <tr><td>How Deep the Father's Love for Us</td><td>Stuart Townend</td></tr>
+                        <tr><td>In Christ Alone</td><td>Keith Getty; Stuart Townend</td></tr>
+                        <tr><td>In His Name</td><td>Charles Wesley; Gavin Perkins; Luke Woodhouse; Nathan Walter</td></tr>
+                        <tr><td>Just As I Am</td><td>Charlotte Elliott; David Moffitt; Sue C. Smith; Travis Cottrell; William Batchelder Bradbury</td></tr>
+                        <tr><td>Revelation Song</td><td>Jennie Lee Riddle</td></tr>
+                        <tr><td>Rock Of Ages</td><td>Augustus Montague Toplady; arr Ruth Buchanan</td></tr>
+                        <tr><td>See Him Coming</td><td>Mark Peterson</td></tr>
+                        <tr><td>Speak O Lord</td><td>Keith Getty; Stuart Townend</td></tr>
+                        <tr><td>There Is A Redeemer</td><td>Melody Green</td></tr>
+                        <tr><td>This Life I Live</td><td>Michael Morrow</td></tr>
+                        <tr><td>Undivided</td><td>Rob Smith</td></tr>
+                        <tr><td>What A Friend We Have In Jesus</td><td>Charles Crozat Converse; David Paxton; Joseph Medlicott Scriven</td></tr>
+                        <tr><td>You Gave Your Life (If Christ be God)</td><td>Steve Tillbrook</td></tr>
+                        <tr><td>You Never Change</td><td> Rob Smith</td></tr>
+                    </tbody></table>
             </section>
         );
     }
